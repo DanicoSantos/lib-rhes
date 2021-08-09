3cda0817249559a5c97239bbbae53f0e141eadd1
Daniel Santos <daniel.santos@octopass.com.br>
2021-08-09 18:22:45 -0300

    feat: add django rest framework to the project

--- libRhes/catalog/admin.py
+++ libRhes/catalog/admin.py
@@ -2,9 +2,22 @@ from django.contrib import admin
 from django.db import models
 from catalog.models import Author, Genre, Book, BookInstance, Languages
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(BookInstance)
-admin.site.register(Languages)
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+
+admin.site.register(Genre)
+admin.site.register(Languages)
--- libRhes/libRhes/settings.py
+++ libRhes/libRhes/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOED_ORIGINS = [
+    'http://localhost:8080',
+]
+
 
 # Application definition
 
@@ -37,12 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+    'djoser',
     'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
