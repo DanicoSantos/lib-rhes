f4fc79af396e42d5d7001e402bc311f34ff1e36d
Daniel Santos <daniel.santos@octopass.com.br>
2021-08-10 12:53:36 -0300

    feat: setup vuex store

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
+
 import Home from '../views/Home.vue'
 import Signup from '../views/Signup.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
@@ -20,6 +23,11 @@ const routes = [
     path: '/sign-up',
     name: 'Signup',
     component: Signup
+  },
+  {
+    path: '/log-in',
+    name: 'Login',
+    component: Login
   }
 ]
 
@@ -28,4 +36,11 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next('/log-in')
+  } else {
+    next()
+  }
+})
 export default router
